b206c7e7806d71809428e2b4f6a87c70503381e5
David Tschida <dmtschida1@gmail.com>
2016-03-28 02:39:56 -0400

    comverted create to using promises

--- test/handlers/CreateNewDevice.js
+++ test/handlers/CreateNewDevice.js
@@ -1,77 +1,37 @@
 'use strict';
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
 
 var assert = require('assert');
 var createNewDevice = require('../../lib/handlers/CreateNewDevice/index');
 
 describe('CreateNewDeviceFunction', function () {
-  it("should return a {status, payload} object", function(done) {
-    createNewDevice.handler({
-        "name": "test_" + Math.random(),
-        "room": "MyLivingRoom",
-        "type": "light"
-    },
-    {
-      succeed: function succeed(data) {
-        assert.ok(data.status);
-        assert.ok(data.payload);
-        done();
-      },
-      error: function error(data) {
-        assert(false, "It should not fail.")
-        done();
-      }
-    });
-  });
-
-  it("should require a name", function(done) {
-    createNewDevice.handler({
-        "room": "MyLivingRoom",
-        "type": "light"
-    },
-    {
-      succeed: function succeed(data) {
-        assert(false, "It should not succeed");
-        done();
-      },
-      error: function error(data) {
-        assert(true, "It should fail.")
-        done();
-      }
-    });
-  });
-
-  it("should require a room", function(done) {
-    createNewDevice.handler({
-        "name": "something",
-        "type": "light"
-    },
-    {
-      succeed: function succeed(data) {
-        assert(false, "It should not succeed");
-        done();
-      },
-      error: function error(data) {
-        assert(true, "It should fail.")
-        done();
-      }
-    });
-  });
-
-
-  it("should require a type", function(done) {
-    createNewDevice.handler({
-      "name": "something",
+  it("should create a simple object", function() {
+    return createNewDevice.handler({
+      "name": "test_" + Math.random(),
       "room": "MyLivingRoom",
-    },
-    {
-      succeed: function succeed(data) {
-        assert(false, "It should not succeed");
-        done();
-      },
-      error: function error(data) {
-        assert(true, "It should fail.")
-        done();
-      }
-    });
+      "type": "light"
+    }).should.be.fulfilled
+    .and.eventually.have.property("status");
+  });
+  it("should require a name", function() {
+    return createNewDevice.handler({
+      "room": "MyLivingRoom",
+      "type": "light"
+    }).should.be.rejected;
+  });
+  it("should require a room", function() {
+    return createNewDevice.handler({
+      "name": "test_" + Math.random(),
+      "type": "light"
+    }).should.be.rejected;
+  });
+  it("should require a type", function() {
+    return createNewDevice.handler({
+      "name": "test_" + Math.random(),
+      "room": "MyLivingRoom",
+    }).should.be.rejected;
   });
 });
