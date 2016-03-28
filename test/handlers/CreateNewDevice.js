0a2ea065b773ca40637bd3581dbc4b8a95f1c15a
David Tschida <dmtschida1@gmail.com>
2016-03-28 03:08:19 -0400

    Added promise for GetDeviceByName

--- test/handlers/CreateNewDevice.js
+++ test/handlers/CreateNewDevice.js
@@ -6,32 +6,45 @@ chai.should();
 
 var assert = require('assert');
 var createNewDevice = require('../../lib/handlers/CreateNewDevice/index');
+var getDeviceByName = require('../../lib/handlers/GetDeviceByName/index');
 
-describe('CreateNewDeviceFunction', function () {
-  it("should create a simple object", function() {
-    return createNewDevice.handler({
-      "name": "test_" + Math.random(),
-      "room": "MyLivingRoom",
-      "type": "light"
-    }).should.be.fulfilled
-    .and.eventually.have.property("status");
+describe("Aws calls", function() {
+  describe('CreateNewDevice', function () {
+    // it("should create a simple object", function() {
+    //   return createNewDevice.handler({
+    //     "name": "test_" + Math.random(),
+    //     "room": "MyLivingRoom",
+    //     "type": "light"
+    //   }).should.be.fulfilled
+    //   .and.eventually.have.property("status");
+    // });
+    it("should require a name", function() {
+      return createNewDevice.handler({
+        "room": "MyLivingRoom",
+        "type": "light"
+      }).should.eventually.be.rejected;
+    });
+    it("should require a room", function() {
+      return createNewDevice.handler({
+        "name": "test_" + Math.random(),
+        "type": "light"
+      }).should.eventually.be.rejected;
+    });
+    it("should require a type", function() {
+      return createNewDevice.handler({
+        "name": "test_" + Math.random(),
+        "room": "MyLivingRoom",
+      }).should.eventually.be.rejected;
+    });
   });
-  it("should require a name", function() {
-    return createNewDevice.handler({
-      "room": "MyLivingRoom",
-      "type": "light"
-    }).should.eventually.be.rejected;
-  });
-  it("should require a room", function() {
-    return createNewDevice.handler({
-      "name": "test_" + Math.random(),
-      "type": "light"
-    }).should.eventually.be.rejected;
-  });
-  it("should require a type", function() {
-    return createNewDevice.handler({
-      "name": "test_" + Math.random(),
-      "room": "MyLivingRoom",
-    }).should.eventually.be.rejected;
-  });
-});
+
+  describe("GetDeviceByName", function() {
+    it("should complete normally", function() {
+      return getDeviceByName.handler({
+        name: "b7aae05e-39e2-4385-85f4-a5914bc33d93"
+      })
+      .should.be.fulfilled
+      .and.eventually.have.property("status");
+    });
+  })
+})
